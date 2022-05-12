e15843792bc34d816d36fd504122685ebd215d4b
Luke <lazarluke02@gmail.com>
2022-05-12 20:24:56 +1000

    csv

--- Reading_Serial_python/map.m
+++ Reading_Serial_python/map.m
@@ -13,28 +13,8 @@ clear
 
 %load and store the data 'radar_202.csv' in raw_data
 %be in form distance, angle
-%raw_data = csvread('Sample1.csv');
-
-%raw_data = importfile('Sample.csv');
-
-%establish the number of angles and ranges in the data
-file = fopen('angledata.txt', 'r');
-
-
-angles = fscanf(file, '%i');
-fclose(file);
-
-file = fopen('lidar.txt', 'r');
-
-time_intervals = fscanf(file, '%i');
-
-fclose(file);
-
-%number_of_data_sets = size(angles);
-
-
-%create the  zero matrix of the same size of raw_data
-%time_intervals = raw_data(:,1);
+angles=csvread('angledata.csv');
+time_intervals = csvread('lidar.csv');
 
 [num_angle_readings, ~] = size(angles);
 [number_lidar_readings, ~] = size(time_intervals);
@@ -45,26 +25,29 @@ while(num_angle_readings ~= number_lidar_readings)
     [number_lidar_readings, ~] = size(time_intervals);
     
     if(num_angle_readings > number_lidar_readings)
-       angles = angles(1:end-1); 
+       angles = angles(1:end-1, :); 
     elseif (num_angle_readings < number_lidar_readings)
-        time_intervals = time_intervals(1:end-1);      
+        time_intervals = time_intervals(1:end-1, :);      
     end
     
     
     
     
 end
-    
 
-maxvalue=max(angles);
-minvalue=min(angles);
+%create the  zero matrix of the same size of raw_data
+azimuth = angles(:,1);
+
+maxvalue=max(azimuth);
+minvalue=min(azimuth);
 difference=maxvalue-minvalue;
 
 
-angles = angles - minvalue;
-angles = angles ./ (difference/100);
-angles = angles .* 0.9;
-angles = angles - 45;
+
+azimuth = azimuth - minvalue;
+azimuth = azimuth ./ (difference/100);
+azimuth = azimuth .* 0.9;
+azimuth = azimuth - 45;
 
 
 
@@ -75,14 +58,14 @@ c=299792458;
 
 %calculate the ranges by using the prior equation, multiplying time x
 %speed, dividing by two and 10^9 to convert to metres
-ranges= time_intervals .* c / (10^13);
+ranges=time_intervals .* c / (10^13);
 
 
 %convert the range and angle to the x coordinate via r*cos(t)
-x= ranges .* cosd(angles);
+x= ranges .* cosd(azimuth);
 
 %convert the range and angle to the y coordinate via r*sin(t)
-y=ranges .* sind(angles);
+y=ranges .* sind(azimuth);
 
 
 scatter(x,y,"*")
