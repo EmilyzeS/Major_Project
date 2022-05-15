eeda0a5f2c94a26e001fac6e1fcc10c8cb303a8c
Emily <esac2470@uni.sydney.edu.au>
2022-05-15 13:27:35 +1000

    updated magnet reading file

--- Reading_Serial_python/magnet.py
+++ Reading_Serial_python/magnet.py
@@ -10,22 +10,61 @@ t = 0
 with open('mag.csv', 'r') as csvfile:
     
     read = csv.reader(csvfile)
-
+    
+    
     for row in read:
-        x.append(row[0])
-        y.append(row[1])
-        z.append(row[2])
+        #These scale factors are the standard deviation of the background noise
+        x.append(int(row[0])/605)
+        y.append(int(row[1])/771)
+        z.append(int(row[2])/479)
         t = t+1
 
-time = np.arange(1,t)
-figure()
-plt.plot(t,x,'r')
-plt.plot(t,y,'b')
-plt.plot(t,z,'g')
+mod = []
+
+means = []
+stdvs = []
+
+#Use this line if unsure where data is static
+#means = [0, 0, 0, 0]
+
+time = np.arange(1,t+1)
+for i in time-1:
+    mod.append(np.sqrt((x[i])**2+(y[i])**2+(z[i])**2))
+
+components = [x,y,z,mod]
+components_names = ['x','y','z','Modulus']
+
+#Change the values based on ploting
+for i in components:
+    
+    means.append(np.mean(i[400:500]))  
+    stdvs.append(np.std(i[400:500]))
+
+plt.figure()
+plt.xlabel('Time')
+plt.ylabel('Magnetic Field Strength')
+plt.plot(time,x,'r', label = 'x')
+plt.plot(time,y,'b', label = 'y')
+plt.plot(time,z,'g',label = 'z')
 plt.legend()
-plt.show()
 
-
+colors = ['b','g','r','purple']
+i = 0
+while i < 4:
+    
     
 
+    plt.figure()
+    plt.xlabel('Time')
+    plt.ylabel("Magnetic Strength")
+    plt.title(f'{components_names[i]}')
+    plt.plot(time,(components[i]- means[i]), c = colors[i])
+
+    #plt.plot(time,(components[i]- means[i])/stdvs[i], c = colors[i])
+    print(f'Mean {components_names[i]}: {means[i]}')
+    print(f'Standard Deviations {components_names[i]}: {stdvs[i]}')
+    i = i+1
+    
+
+plt.show()
 
