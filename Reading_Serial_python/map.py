bf24b5af16352560a9b97f8899d705370d3240ec
Luke Lazar <74656988+lazarr1@users.noreply.github.com>
2022-05-24 11:47:10 +1000

    comments

--- Reading_Serial_python/map.py
+++ Reading_Serial_python/map.py
@@ -2,12 +2,15 @@ from re import S
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import delete
+import send_to_c
 from mpl_toolkits import mplot3d
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import KMeans
 
+#function to convert gyro data to angles
 def convertAnglesGyro(gyro_velocities):
+    
+    #create an azimuth and levation array
     servo_positions = pd.DataFrame(columns = ['Azimuth', 'Elevation'])
     servo_positions['Azimuth'] = np.zeros(len(gyro_velocities))
     servo_positions['Elevation'] = np.zeros(len(gyro_velocities))
@@ -17,6 +20,7 @@ def convertAnglesGyro(gyro_velocities):
     delta_time = []
     delta_time.append(0)
     
+    #cALCULATE the time between different intervals
     i = 1
     while i < len(times):
         if times[i] > times[i-1]:
@@ -29,13 +33,15 @@ def convertAnglesGyro(gyro_velocities):
     delta_time.pop(0)
     delta_time.append(0)
 
+    #convert time to seconds
     gyro_velocities['Delta Time'] = np.array(delta_time)
     gyro_velocities['Delta Time'] = gyro_velocities['Delta Time'] / (24 * 10**6)
 
-
+    #starting angles
     azimuths = [0]
     elevations = [0]
     
+    #perform integration on angular velocity to find angles
     i = 1
     while i  < len(delta_time):
         azimuths.append(azimuths[i-1] + gyro_velocities['Delta Time'][i-1] * gyro_velocities['xvel'][i-1])
@@ -45,21 +51,25 @@ def convertAnglesGyro(gyro_velocities):
     servo_positions['Azimuth'] = np.array(azimuths)
     servo_positions['Elevation'] = np.array(elevations)
 
+    #convert to degrees
     servo_positions['Azimuth'] *= 180/np.pi
     servo_positions['Elevation'] *= 180/np.pi
 
 
     return servo_positions
 
+#function to convert elevation azimuth and sitance to x,y,z coordinates
 def polarToRectangularGyro(ranges, servo_angles):
     hits = pd.DataFrame(columns = ['X', 'Y','Z','D'])
     cosVals = np.cos(servo_angles['Elevation'])
     sinVals = np.sin(servo_angles['Elevation'])
+    #find distance and z heights
     hits['Z'] = ranges["Ranges"] * sinVals
     hits['D'] = ranges["Ranges"] * cosVals
     print('old')
     print(max(servo_angles['Azimuth'] * 180/np.pi), min(servo_angles['Azimuth'] * 180/np.pi))
     
+    #calibrate angles to centre
     max_az = max(servo_angles['Azimuth'])
     min_az = min(servo_angles['Azimuth'])
     diff = max_az- min_az
@@ -70,6 +80,7 @@ def polarToRectangularGyro(ranges, servo_angles):
     print('new')
     print(max(servo_angles['Azimuth'] * 180/np.pi), min(servo_angles['Azimuth'] * 180/np.pi))
 
+    #find x and y coordinates
     cosVals = np.cos(servo_angles['Azimuth'])
     sinVals = np.sin(servo_angles['Azimuth'])
     hits['X'] = hits['D'] * cosVals
@@ -84,25 +95,30 @@ def polarToRectangularGyro(ranges, servo_angles):
 
 
 def map(x_offset,y_offset):
+    #read lidar
     lidar_readings = pd.read_csv ('lidar.csv', header = None)
     ranges = lidar_readings/(24*10**(3))
     ranges.set_axis(["Ranges"], axis = 1, inplace = True)
 
+    #read and save gyro
     gyro_velocities = pd.read_csv('gyro.csv',  header = None)
     gyro_velocities.set_axis(["xvel", "yvel","zvel","time"], axis = 1, inplace = True)
-    #gyro_velocities['xvel'] -= x_offset
-    #gyro_velocities['yvel'] -= y_offset
+    gyro_velocities['xvel'] -= x_offset
+    gyro_velocities['yvel'] -= y_offset
 
+    #find angles
     servo_angles = convertAnglesGyro(gyro_velocities) 
 
+    #find x,y,z
     hits = polarToRectangularGyro(ranges, servo_angles)
     hits.dropna()
 
-    #hits = hits[hits['Y'] > 0.5]
-    #hits = hits[hits['Y'] < 1.5]
+    #filter boundaries
+    hits = hits[hits['Y'] > 0.5]
+    hits = hits[hits['Y'] < 1.5]
 
 
-
+    #filter
     model = DBSCAN(eps = 0.1, min_samples = 20).fit(hits[['X','Y']])
     colors = model.labels_
 
@@ -112,6 +128,7 @@ def map(x_offset,y_offset):
 
     clusters = []
 
+    #perform kmeans filter
     for cluster in range(numClusters):
         cluster_hits = pd.DataFrame(hits)[model.labels_ == cluster]
         kmeans = KMeans(n_clusters= 1, random_state=0).fit(cluster_hits)
@@ -133,15 +150,17 @@ def map(x_offset,y_offset):
         plt.scatter(point[0], point[1] ,marker = 'o')
     
 
-
+    #create figure
     fig = plt.figure(figsize=(12, 12))
     ax = fig.add_subplot(projection='3d')
     ax.scatter(hits['X'], hits['Y'],hits['Z'])
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
     ax.set_zlabel('Z Label')
+    plt.show()
     message, format = prepareData(hits_xy)
-    delete.sendData(message, format, "object")
+    send_to_c.sendData(message, format, "object")
+
 
 
 # def prepareData(data):
@@ -156,8 +175,11 @@ def map(x_offset,y_offset):
 #     return messages
 
 def prepareData(data):
+
+    #prepare data for c
     message = ""
     i = 1
+    #in format (x1,y1) (x2,y2) etc
     for hit in data:
         message += "(" + str(round(hit[0]*100)) + ", " + str(round(hit[1]*100)) + ")"
         i += 1
